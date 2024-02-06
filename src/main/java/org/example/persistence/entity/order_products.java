f97422852b5bd33e90f0f2f6a0eca7ef87e8d6ce
Daakemii <daakemii@gmail.com>
2024-02-06 10:56:44 -0300

    Adicionado order_products entity

---
+++ src/main/java/org/example/persistence/entity/order_products.java
@@ -0,0 +1,27 @@
+package org.example.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name="order_products")
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class order_products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_products_id")
+    private UUID orderProductsId;
+    @Column(name = "order_id")
+    private UUID orderId;
+    @Column(name = "product_id")
+    private UUID product_id;
+    private Integer quantity;
+    private String comment;
+}
