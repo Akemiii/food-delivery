fbee27dbf310f4df6791b9573160f2e444fa1df4
Daakemii <daakemii@gmail.com>
2024-02-06 10:49:56 -0300

    Adicionado order entity

---
+++ src/main/java/org/example/persistence/entity/Order.java
@@ -0,0 +1,30 @@
+package org.example.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name="category")
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_id")
+    private UUID orderId;
+    @Column(name = "restaurant_id")
+    private UUID restaurantId;
+    private String address;
+    private String name;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "total_value")
+    private String totalValue;
+    private Boolean status;
+}
