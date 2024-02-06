11d3746a75e4d04305adebb6d8b2397b631e087b
Daakemii <daakemii@gmail.com>
2024-02-06 10:31:13 -0300

    Adicionado category entity

---
+++ src/main/java/org/example/persistence/entity/Category.java
@@ -0,0 +1,23 @@
+package org.example.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name="category")
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "category_id")
+    private UUID categoryId;
+    private String title;
+}
