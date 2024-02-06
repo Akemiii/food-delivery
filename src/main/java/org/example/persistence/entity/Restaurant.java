fc98aba27cc8c4ca4fca3e445e95252a51c9e826
Daakemii <daakemii@gmail.com>
2024-02-06 10:27:04 -0300

    Adicionado restaurant entity

---
+++ src/main/java/org/example/persistence/entity/Restaurant.java
@@ -0,0 +1,34 @@
+package org.example.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Table(name="restaurant")
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "restaurant_id")
+    private UUID restaurantId;
+    private String name;
+    private String description;
+    private Boolean status;
+    private BigDecimal delivery_tax;
+    private UUID category_id;
+    private String city;
+    private String state;
+    private String neighborhood;
+    private String street;
+    private String number;
+    private String complement;
+    private String reference;
+
+}
