b8020db31807759919d21a0a35394cac6720bc68
Daakemii <daakemii@gmail.com>
2024-02-08 23:02:38 -0300

    Mapeado a entidade

--- src/main/java/org/example/persistence/entity/Additionalitems.java
+++ src/main/java/org/example/persistence/entity/Additionalitems.java
@@ -1,7 +1,33 @@
 package org.example.persistence.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.SimpleTimeZone;
+import java.util.UUID;
+
+@Table(name = "additional_items")
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Additionalitems {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+    private String name;
+    private String description;
+    private String image;
+    private BigDecimal price;
 
+    @ManyToOne
+    @JoinColumn(name = "choice_id")
+    private Choice choice;
 
 }
