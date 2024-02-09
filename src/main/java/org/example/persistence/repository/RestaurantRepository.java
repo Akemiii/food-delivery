ca9c53e02833471e49fe3084e18b55dc2ec0fb6d
Daakemii <daakemii@gmail.com>
2024-02-09 10:49:11 -0300

    .

--- src/main/java/org/example/persistence/repository/RestaurantRepository.java
+++ src/main/java/org/example/persistence/repository/RestaurantRepository.java
@@ -3,7 +3,11 @@ package org.example.persistence.repository;
 import org.example.persistence.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
+
+    List<Restaurant> findByCategory_categoryId(UUID categoryId);
+
 }
